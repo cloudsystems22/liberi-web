30f05df7c6f7870ae990f1b67cc1dd45c909d7c0
cloudsystems22 <cloudsystems22@gmail.com>
2020-04-14 18:12:36 -0300

    Arquivo inicial liberi

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Liberi móbile</title>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,400;0,700;0,800;1,400;1,600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/pallet.css">
+</head>
+<body>
+    <div id='div-logo'>
+        <h1><span id='span-logo-left'>Liberi</span><span id='span-logo-right'>móvil</span></h1>
+    </div>
+    <nav class='flex'>
+        <div class='br-primary-transparent' id='menu-left'></div>
+        <div class='bg-secundary-transparent' id='menu-right'>
+            <div class='flex'>
+                <div class='item'>
+                    <a href="#!" onclick="toggleSidebar()"><img src="images/menu-abrir-white.png" alt="menu" id='img-menu'> Menu</a>
+                </div>
+                <div class='item'>
+                    <ul id='ul-menu'>
+                        <li><a href="#!">Home</a></li>
+                        <li><a href="#!">Contato</a></li>
+                        <li><a href="#!">Suporte</a></li>
+                        <li><a href="#!">Faça seu cadastro</a></li>
+                        <li><a href="#!">Parceiros</a></li>
+                    </ul>
+                </div>
+
+            </div>
+        </div>
+    </nav>
+    <section id="home">
+
+    </section>
+
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+<script src="scripts/my-menu.js"></script>
+</body>
+</html>
---
+++ scripts/my-menu.js
@@ -0,0 +1,21 @@
+$(document).ready(function (){
+    //document.getElementById('ul-menu').style.display = 'none';
+});
+
+function toggleSidebar(){
+    document.getElementById('menu-left').classList.toggle('active');
+    document.getElementById('ul-menu').classList.toggle('active');
+    let menuRight = document.getElementById('menu-right').classList.toggle('active');
+    console.log(menuRight);
+    
+    if(menuRight){
+        document.getElementById('img-menu').src = 'images/fechar-white.png';
+        //document.getElementById('ul-menu').style.display = 'inline-block';
+        //document.getElementById('img-menu').setAttribute('src', 'images/fechar-white.png');
+        //alert(menuRight);
+    }
+    else {
+        document.getElementById('img-menu').setAttribute('src', 'images/menu-abrir-white.png');
+        //document.getElementById('ul-menu').style.display = 'none';
+    }
+}
